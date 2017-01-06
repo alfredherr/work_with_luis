c8b00293899abef75cbfa1461b53961ecf74e582
Luis Herrera <donluisherrera@gmail.com>
2017-01-06 06:35:22 -0700

    added functionality to dynamically get pattern

--- evan.pl
+++ evan.pl
@@ -1,12 +1,16 @@
 #!/usr/bin/perl -w
 
 my $counter = 0;
-while(<>){
+my $file = $ARGV[0];
+open(FILE,$file) or die "can't open $file\n";
+
+my $pattern = $ARGV[1]; 
+while(<FILE>){
 	chomp;
 	my @rec = split//;
 	foreach $letter (@rec){
-		$counter = $counter + 1 if( $letter eq '0' )
+		$counter = $counter + 1 if( $letter eq $pattern )
 	}
 }
 
-print "There are $counter 0s in the file\n";
+print "There are $counter $pattern in the file\n";
