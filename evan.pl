a71440c2fbf03db007da01cd3faed5ac4a1a3caa
Alfredo Herrera <alfredherr@gmail.com>
2017-01-10 05:18:12 -0700

    added functionality to search for any pattern of any size

--- evan.pl
+++ evan.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/perl -w
 
-my $counter = 0;
+my $times_pattern_was_found = 0;
 my $file = $ARGV[0];
+my $pattern = $ARGV[1]; 
+
 open(FILE,$file) or die "can't open $file\n";
 
-my $pattern = $ARGV[1]; 
-while(<FILE>){
-	chomp;
-	my @rec = split//;
-	foreach $letter (@rec){
-		$counter = $counter + 1 if( $letter eq $pattern )
+while(my $line = <FILE>){
+	chomp $line;
+	my @rec = split/\,/, $line;
+	foreach $word (@rec){
+		if( $word eq $pattern ){
+			#increment my counter
+			$times_pattern_was_found = $times_pattern_was_found + 1; 
+		}
 	}
 }
 
-print "There are $counter $pattern in the file\n";
+print "There are $times_pattern_was_found $pattern in the file\n";
